cc67e17cc6d54a5bacf8740e529fbc40ab3b6ca9
SonSWE <dangtiensonuh@gmail.com>
2021-09-24 22:54:31 +0700

    first commit

---
+++ WebQuanLyQuanCafe/Controllers/LoginController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebQuanLyQuanCafe.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        [HttpGet]
+        public ActionResult Login()
+        {
+            if (Session["usernameLogin"] != null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            if(username == "admin" && password == "admin")
+            {
+                Session["usernameLogin"] = username;
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+    }
+}
